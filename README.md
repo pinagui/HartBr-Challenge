f1a0ec4437cfbd6ea349b4ac58f1e8063a7fac83
pinagui <guilhermepinaramos@gmail.com>
2023-03-28 11:57:56 -0300

    feat: add branch with steps comments

---
+++ README.md
@@ -0,0 +1,22 @@
+importando a biblioteca base64 para decodificar o array de bytes em base64 => https://docs.python.org/3/library/base64.html
+
+format -> http://www.java2s.com/example/android/java.lang/convert-byte-array-to-hex-string-using-stringformat02x-b-0xff.html
+
+lsb-first python -> https://www.phind.com/search?cache=d9dbf325-7217-401f-a5c7-dc1524a29fb4
+
+with open -> para leitura do arquivo txt
+data está guardando a leitura do arquivo,
+
+[x] escolha qualquer linguagem e consulte quaisquer informações/sites que achar necessário
+
+[x] ler o conteúdo de um arquivo de texto ( o caminho pode ser inserido da melhor forma escolhida) cujo conteúdo possui o formato {data:"Array de Bytes em Base64"}, sem considerar as aspas;
+
+[x] ler o conteúdo do Array de Bytes e imprima/mostre o conteúdo dos bytes em formato Hexadecimal;
+
+[x] interprete os números que são representados por 4 bytes LSB-First com sinal e Imprima/Mostra na tela;
+
+[x] Ordene os números de forma crescente;
+
+[x] Imprima/Mostre os números ordenados na tela da melhor forma escolhida;
+
+[x] Não há limitação direta para o tamanho do array nem do arquivo de texto;
--- challenge.py
+++ challenge.py
@@ -1,22 +1,40 @@
 import base64
 import struct
+# allow create package bytes to decode variables int, float, string, values
 
 with open('data.txt', 'r') as f:
     data = f.read().split(":")[1]
+print(data)
 
 decoded = base64.b64decode(data)
 
-arrayDecoded = [format(byte, "02x") for byte in decoded]
+print(decoded)
+#b'2\x00\x00\x00\xfd\xff\xff\xff\x00\x00\x00\x00'
 
+arrayDecoded = [format(byte, "02x") for byte in decoded]
+# convert string to array
+# format take off "x" from array with only 2 characters length
+print(arrayDecoded)
+# ['32', '00', '00', '00', 'fd', 'ff', 'ff', 'ff', '00', '00', '00', '00']
 arrayBytes = ''.join(arrayDecoded).upper()
+# join to concat 
+
 print("Array de Bytes:", arrayBytes)
 
+#LSB-First order bytes values are stored in memory or transmitted over a network
+# least significant byte value comes first
+# Least Significant Bit first
+# This means that the least significant bit (on the right) contains the most important numerical information and the most significant bit (on the left) contains the sign information.
 numberBytes = []
 
 for i in range(0, len(decoded), 4):
+  # range -> starts in 0, ends on length decoded and advance 4 by 4
   numberBytes.append(struct.unpack('<i', decoded[i:i+4])[0])
-
+  # struct.unpack covert piece of byte array to signal lsb-first format
 print("Números Lidos:", numberBytes)
-
+  # < -> LSB First
+  # i -> int number with 4 bytes
+  # [i:i+4] -> advance 4 by 4
 numberBytes.sort()
 print("Números Ordenados:", numberBytes)
+
